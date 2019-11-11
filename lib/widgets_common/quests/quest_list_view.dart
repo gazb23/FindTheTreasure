bce4429b27551e67d6920dce0a2fbb5fe2ba49f9
Gareth Beall <gareth.beall@gmail.com>
2019-11-11 21:24:28 +1000

    Working on explore page

--- lib/widgets_common/quests/quest_list_view.dart
+++ lib/widgets_common/quests/quest_list_view.dart
@@ -12,24 +12,77 @@ class QuestListView extends StatelessWidget {
         horizontal: 10.0,
         vertical: 10.0,
       ),
-      height: 250.0,
+      height: 325.0,
       width: double.infinity,
       child: Column(
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Placeholder(
-              
-              color: Colors.grey,
-              fallbackHeight: 150.0,
+          Container(
+            child: Column(
+              children: <Widget>[
+                Stack(
+                  children: <Widget>[
+                    ClipRRect(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10.0),
+                        topRight: Radius.circular(10.0),
+                      ),
+                      child: Image.asset('images/explore/wild_mountain.jpg'),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(10.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            'Waterfall Quest',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30.0,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Icon(
+                            Icons.favorite_border,
+                            color: Colors.white,
+                            size: 30.0,
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ],
             ),
           ),
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
             child: Row(
-              
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                Text('Waterfall Quest')
+                Container(
+                  padding: EdgeInsets.all(5.0),
+                  decoration: BoxDecoration(
+                      color: Colors.orangeAccent,
+                      borderRadius: BorderRadius.circular(10.0)),
+                  child: Text(
+                    'Easy',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                Container(
+                  width: 200.0,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Image.asset('images/ic_diamond.png'),
+                      Text('50'),
+                      Image.asset(
+                        'images/explore/key.png',
+                        height: 40.0,
+                      ),
+                      Text('Water')
+                    ],
+                  ),
+                )
               ],
             ),
           )
