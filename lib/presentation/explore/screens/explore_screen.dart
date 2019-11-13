bf94979b86ba2d9a3a429147630fcfea7c163d38
Gareth Beall <gareth.beall@gmail.com>
2019-11-13 21:36:03 +1000

    added font and updated questlistview

--- lib/presentation/explore/screens/explore_screen.dart
+++ lib/presentation/explore/screens/explore_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:find_the_treasure/widgets_common/quests/quest_list_view.dart';
 import 'package:flutter/material.dart';
 
@@ -6,33 +5,57 @@ class ExploreScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-          child: Scaffold(
+      child: Scaffold(
+        backgroundColor: Colors.black12,
         appBar: AppBar(
-          centerTitle: true,          
+          centerTitle: true,
           title: FlatButton(
-            onPressed: () => showDialog(
-              context: context,
-              builder: (context) => AlertDialog(
-                backgroundColor: Colors.white,
-                content: Text('You clicked me'),
-                title: Text('TREASURE!!'),
-              )
-            ),
-            child: Image.asset('images/ic_treasure.png')),
+              onPressed: () => showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                        backgroundColor: Colors.white,
+                        content: Text('You clicked me'),
+                        title: Text('TREASURE!!'),
+                      )),
+              child: Image.asset('images/ic_treasure.png')),
         ),
-        body: Stack(
-          children: <Widget>[
-            Container(            
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("images/bckgrnd.png"),
-                    fit: BoxFit.cover),
+        body: 
+        // Container(
+        //   decoration: BoxDecoration(
+        //     image: DecorationImage(
+        //         image: AssetImage("images/bckgrnd.png"), fit: BoxFit.cover, ),
+        //   ),
+        //   child:
+           ListView(
+            children: <Widget>[
+              QuestListView(
+                title: 'Mountain Quest',
+                image: AssetImage('images/explore/wild_mountain.jpg'),
+                diamondCount: 50,
+                difficultyColor: Colors.greenAccent,
+                difficultyTitle: 'Easy',
+                keyCount: 1,
               ),
-            ),
-            QuestListView(),
-          ],
+              QuestListView(
+                diamondCount: 125,
+                difficultyColor: Colors.redAccent,
+                difficultyTitle: 'Hard',
+                image: AssetImage('images/explore/Coolum.jpg'),
+                keyCount: 3,
+                title: 'Beach Quest',
+              ),
+              QuestListView(
+                diamondCount: 225,
+                difficultyColor: Colors.orangeAccent,
+                difficultyTitle: 'moderate',
+                image: AssetImage('images/explore/alex_heads.jpg'),
+                keyCount: 1,
+                title: 'Ultimate Beach Quest',
+              )
+            ],
+          ),
         ),
-      ),
+      
     );
   }
 }
--- lib/widgets_common/quests/quest_list_view.dart
+++ lib/widgets_common/quests/quest_list_view.dart
@@ -1,85 +1,96 @@
 import 'package:flutter/material.dart';
 
 class QuestListView extends StatelessWidget {
+  final AssetImage image;
+  final String title;
+  final Color difficultyColor;
+  final String difficultyTitle;
+  final int diamondCount;
+  final int keyCount;
+
+  const QuestListView({
+    Key key,
+    @required this.title,
+    @required this.difficultyColor,
+    @required this.difficultyTitle,
+    @required this.diamondCount,
+    @required this.keyCount,
+    @required this.image,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      margin: EdgeInsets.symmetric(
-        horizontal: 10.0,
-        vertical: 10.0,
-      ),
-      height: 325.0,
-      width: double.infinity,
+    return Card(
+      margin: EdgeInsets.all(10.0),
+      color: Colors.black87,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(15.0))),
       child: Column(
         children: <Widget>[
           Container(
-            child: Column(
-              children: <Widget>[
-                Stack(
-                  children: <Widget>[
-                    ClipRRect(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(10.0),
-                        topRight: Radius.circular(10.0),
-                      ),
-                      child: Image.asset('images/explore/wild_mountain.jpg'),
-                    ),
-                    Container(
-                      padding: EdgeInsets.all(10.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text(
-                            'Waterfall Quest',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 30.0,
-                                fontWeight: FontWeight.bold),
-                          ),
-                          Icon(
-                            Icons.favorite_border,
-                            color: Colors.white,
-                            size: 30.0,
-                          )
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ],
+            height: 200.0,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              image: DecorationImage(
+                  image: image,
+                  fit: BoxFit.fill,
+
+                  colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.8), BlendMode.dstATop),
+                  alignment: Alignment.topCenter),
+            ),
+            child: ListTile(
+              contentPadding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
+              title: Text(
+                title,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'JosefinSans'),
+              ),
+              trailing: Icon(
+                Icons.favorite_border,
+                color: Colors.white,
+                size: 30.0,
+              ),
             ),
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+            padding:
+                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
                 Container(
-                  padding: EdgeInsets.all(5.0),
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 10.0, vertical: 3.0),
                   decoration: BoxDecoration(
-                      color: Colors.orangeAccent,
-                      borderRadius: BorderRadius.circular(10.0)),
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: difficultyColor),
                   child: Text(
-                    'Easy',
+                    difficultyTitle,
                     style: TextStyle(color: Colors.white),
                   ),
                 ),
                 Container(
-                  width: 200.0,
+                  width: 150.0,
                   child: Row(
+                    mainAxisSize: MainAxisSize.max,
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: <Widget>[
                       Image.asset('images/ic_diamond.png'),
-                      Text('50'),
+                      Text(
+                        diamondCount.toString(),
+                        style: TextStyle(color: Colors.white),
+                      ),
                       Image.asset(
                         'images/explore/key.png',
                         height: 40.0,
                       ),
-                      Text('Water')
+                      Text(
+                        keyCount.toString(),
+                        style: TextStyle(color: Colors.white),
+                      ),
                     ],
                   ),
                 )
